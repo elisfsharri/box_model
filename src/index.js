ed07d64e1e97a01bed0b52d0f9bb761aad3fa2da
Elis Fsharri <elisfsharri@yahoo.com>
2020-11-24 18:56:06 +0100

    modified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Boxroom from './boxroom ';
+import Boxroom from './boxroom';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
