ac06d6ba38c0ec5609dcabce1d40ce4a3df03c06
Elis Fsharri <elisfsharri@yahoo.com>
2021-02-02 16:07:10 +0100

    ...

--- src/box.js
+++ src/box.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Box=({item,description})=>{
   return(
-    <div className='bg-lightest-blue dim dib pa5 ma5 grow bw2 shadow-5 tc w-20 min-h-100l'>
+    <div className='bg-lightest-blue dim dib pa5 ma4 grow bw2 shadow-5 tc w-20 min-h-100l'>
       <h4 className='h3'>{item}</h4>
       <p className='h3'>{description}</p>
     </div>
